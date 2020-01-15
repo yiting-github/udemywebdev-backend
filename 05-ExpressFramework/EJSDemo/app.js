496f653efe94d096e2ce9b0883f530afe9805cce
yiting-github <w6812763cm@gmail.com>
2020-01-15 23:14:46 +0800

    ejs if and loop statement

--- 05-ExpressFramework/EJSDemo/app.js
+++ 05-ExpressFramework/EJSDemo/app.js
@@ -5,6 +5,15 @@ app.get("/", function(req, res){
     res.render("home.ejs");
 });
 
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post1", author: "Ray"},
+        {title: "My adorable pet bunny", author: "Charlie"},
+        {title: "Can you believe this pomsky?", author: "Crown"}
+    ];
+    res.render("posts.ejs", { posts: posts });
+});
+
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
     res.render("love.ejs",{ thingVar: thing});
