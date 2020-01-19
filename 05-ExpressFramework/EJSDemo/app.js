526cfca3d41c3b414a10af03499ea41299702de0
yiting-github <w6812763cm@gmail.com>
2020-01-20 00:12:22 +0800

    EJSDemo assets and partials

--- 05-ExpressFramework/EJSDemo/app.js
+++ 05-ExpressFramework/EJSDemo/app.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var app = express();
 
+// serve public contents
+app.use(express.static("public"));
+// tell express  use ejs file ahead of time
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/posts", function(req, res){
@@ -11,12 +16,12 @@ app.get("/posts", function(req, res){
         {title: "My adorable pet bunny", author: "Charlie"},
         {title: "Can you believe this pomsky?", author: "Crown"}
     ];
-    res.render("posts.ejs", { posts: posts });
+    res.render("posts", { posts: posts });
 });
 
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs",{ thingVar: thing});
+    res.render("love",{ thingVar: thing});
 });
 
 app.listen("3000", function(){
