53096ffb88a8cad95d3350645a7f9501d2c7baf9
Angel Chang <w6812763cm@gmail.com>
2020-02-10 14:19:54 +0000

    add campgrounds GET and POST

--- 07-YelpCamp/v1/app.js
+++ 07-YelpCamp/v1/app.js
@@ -1,20 +1,38 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds =[
+{name: "Joshua tree", image: "https://www.reserveamerica.com/webphotos/racms/articles/images/635a0b9d-a566-46cb-acf7-c942b889efea_image2_goblin-valley-state-park-yurts.jpg"},
+{name: "Yellowstone", image: "https://www.reserveamerica.com/webphotos/GA/pid530195/sid258569/3/540x360.jpg"},	
+{name: "Petrified Forest", image: "https://www.reserveamerica.com/webphotos/CO/pid50035/sid1774/0/540x360.jpg"}	
+];
+	
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	var campgrounds =[
-	{name: "Joshua tree", image: "https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c7d2f7dd29e45c25a_340.jpg"},
-	{name: "Yellowstone", image: "https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c7d2f7dd29e45c25a_340.jpg"},	
-	{name: "Petrified Forest", image: "https://pixabay.com/get/52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c7d2f7dd29e45c25a_340.jpg"}	
-	]
-	
 	res.render("campgrounds", {campgrounds: campgrounds});
 });
+
+app.post("/campgrounds", function(req, res){
+	// get data from form and add campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
+	// redirect back to campgrounds page
+	res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+	res.render("new");
+});
+
 
 app.listen(3000, function(){
 	console.log("The YelpCamp server is running!");
