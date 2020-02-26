73be7db8f2eef8591fc3062e6192bf0ea0989e3d
Angel Chang <w6812763cm@gmail.com>
2020-02-26 15:06:31 +0000

    add show page and new mongoose findById

--- 07-YelpCamp/v2/app.js
+++ 07-YelpCamp/v2/app.js
@@ -9,15 +9,18 @@ mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 //Schema Setup
 var campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 
 // compile to model
 
 var Campground = mongoose.model("Campground", campgroundSchema)
+
 // Campground.create(
-// 	{ name: "Yellowstone",
-// 	 image: 	"https://www.reserveamerica.com/webphotos/GA/pid530195/sid258569/3/540x360.jpg"
+// 	{ name: "Joshua tree",
+// 	 image: "https://www.reserveamerica.com/webphotos/racms/articles/images/635a0b9d-a566-46cb-acf7-c942b889efea_image2_goblin-valley-state-park-yurts.jpg",
+// 	 description: "Winter is the busy season for camping at this southern park. Enjoy the beauty of the Joshua trees without the sweltering heat."
 // 	}, function(err, campground){
 // 		if(err){
 // 			console.log(err);
@@ -26,7 +29,6 @@ var Campground = mongoose.model("Campground", campgroundSchema)
 // 			console.log(campground);
 // 		}
 // 	});
-
 
 
 
@@ -40,22 +42,26 @@ app.get("/", function(req, res){
 	res.render("landing");
 });
 
+
+// INDEX Route - all campgrounds
 app.get("/campgrounds", function(req, res){
 	// Get all the data from DB
 	Campground.find({}, function(err, allCampgrounds){
 		if(err){
 			console.log(err);
-		} else {
-			res.render("campgrounds", {campgrounds: allCampgrounds});
+		} else {		
+			res.render("index", {campgrounds: allCampgrounds});
 		}
 	});
 });
 
+//CREATE -add new campground to DB
 app.post("/campgrounds", function(req, res){
 	// get data from form and add campgrounds array
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image};
+	var desc = req.body.desc;
+	var newCampground = {name: name, image: image, description: desc};
 	// Create a new campground and save to DB
 	Campground.create(newCampground, function(err, newlyCreated){
 		if(err){
@@ -68,10 +74,25 @@ app.post("/campgrounds", function(req, res){
 	
 });
 
+// NEW - a form to create
 app.get("/campgrounds/new", function(req, res){
 	res.render("new");
 });
 
+
+// SHOW - shows more info about one camground
+app.get("/campgrounds/:id", function(req, res){
+	// find the campground with ID
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if (err){
+			console.log(err);
+		} else {
+			// render show template with that campground
+			res.render("show", {campground: foundCampground});
+		}
+	});
+	
+})
 
 app.listen(3000, function(){
 	console.log("The YelpCamp server is running!");
