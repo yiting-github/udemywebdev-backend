0657909b165416866db3d85249c7a17525a80ced
Angel Chang <w6812763cm@gmail.com>
2020-03-07 16:11:51 +0000

    SHOW ROUTE

--- 09-RESTfulRouting/RESTfulBlogApp/app.js
+++ 09-RESTfulRouting/RESTfulBlogApp/app.js
@@ -56,6 +56,18 @@ app.post("/blogs", function(req, res){
 	});
 	
 });
+
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.render("show", {blog: foundBlog});
+		}
+		
+	});
+});
 app.listen(3000, function(){
 	console.log("RESTful Blog is running!");
 });
