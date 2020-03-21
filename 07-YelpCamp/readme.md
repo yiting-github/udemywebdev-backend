2fb6615e92f990b25cae5bea19a39f8bd9d0fd68
Angel Chang <w6812763cm@gmail.com>
2020-03-21 12:13:27 +0000

    v4 add comments new form

--- 07-YelpCamp/readme.md
+++ 07-YelpCamp/readme.md
@@ -12,3 +12,23 @@
 ## Add Seeds File
 * Add a seed.js file
 * Run the seeds file every time the server starts
+
+## Add the Comment model
+* Make our errors go away!
+	* new file: models/comment.js
+	* Embedding ID:add comments to campgroundSchema
+* Display comments on camground show page
+
+## Comment New/Create
+* Discuss nested routes
+* Add the comment new and create routes
+
+## Routes
+| Name   | Routes                        | HTTP Verb | Comments                                                             |
+|--------|-------------------------------|-----------|----------------------------------------------------------------------|
+| Index  | /campgrounds                  | GET       | View all campgrounds                                                 |
+| New    | /campgrounds/new              | GET       | a form to create the new campground                                  |
+| Create | /campgrounds                  | POST      | add a new campground, then redirect somewhere                        |
+| Show   | /campgrounds/:id              | GET       | Show details about one specific campground                           |
+| New    | /campgrounds/:id/comments/new | GET       | a form to create the new command about specific campground           |
+| Create | /campgrounds/:id/comments     | POST      | add a new comment about specific campground, then redirect somewhere |
---
+++ 07-YelpCamp/v4/app.js
@@ -0,0 +1,93 @@
+var express    = require("express"),
+	app        = express(),
+	bodyParser = require("body-parser"),
+	mongoose   = require("mongoose"),
+	Campground = require("./models/campground"),
+	seedDB     = require("./seeds");
+
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+seedDB();
+
+
+	
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+
+// INDEX Route - all campgrounds
+app.get("/campgrounds", function(req, res){
+	// Get all the data from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		} else {		
+			res.render("campgrounds/index", {campgrounds: allCampgrounds});
+		}
+	});
+});
+
+//CREATE -add new campground to DB
+app.post("/campgrounds", function(req, res){
+	// get data from form and add campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var desc = req.body.desc;
+	var newCampground = {name: name, image: image, description: desc};
+	// Create a new campground and save to DB
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			// redirect back to campgrounds page
+			res.redirect("/campgrounds");
+		}
+	});
+	
+});
+
+// NEW - a form to create
+app.get("/campgrounds/new", function(req, res){
+	res.render("campgrounds/new");
+});
+
+
+// SHOW - shows more info about one camground
+app.get("/campgrounds/:id", function(req, res){
+	// find the campground with ID
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+		if (err){
+			console.log(err);
+		} else {
+			console.log(foundCampground);
+			// render show template with that campground
+			res.render("campgrounds/show", {campground: foundCampground});
+		}
+	});
+
+})
+
+// =======================
+// COMMENTS ROUTES
+// =======================
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+	// find campground by id
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("comments/new", {campground: campground});
+		}
+	});
+	
+});
+ 
+
+app.listen(3000, function(){
+	console.log("The YelpCamp server is running!");
+});
